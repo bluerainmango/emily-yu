aad697edc6848212f004d73e7838e72ca9623c11
Emily Yu <bluerainmango@gmail.com>
2020-04-02 08:21:47 -0700

    production build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b84a0de48508c30704481cde7a8867e3.js"
+  "/precache-manifest.dca823171fb4b5efcd8500052f6b36a8.js"
 );
 
 self.addEventListener('message', (event) => {
