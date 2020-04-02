613361e245f7424c7db9be9d65b83d05b1d80f82
Emily Yu <bluerainmango@gmail.com>
2020-04-01 19:21:28 -0700

    revised Experience card-link's right margin

--- src/components/top-nav/top-nav.component.jsx
+++ src/components/top-nav/top-nav.component.jsx
@@ -41,9 +41,6 @@ export const TopNav = props => (
         outDuration: 200,
         preventScrolling: true
       }}
-      // data-target="slide-out"
-      className="sidenav-trigger show-on-large"
-      // sidenav={SideNavMenu(false)}
     >
       <NavItem id="profile-mobile">
         <span>bluerainmango@gmail.com</span>
@@ -78,55 +75,6 @@ export const TopNav = props => (
         <Icon>insert_drive_file</Icon>
         Resume
       </NavItem>
-      {/* <NavItem>{SideNavMenu(false)}</NavItem> */}
-      {/* <NavItem
-        className="profile"
-        user={{
-          // background: { profileImg },
-          // background: "https://placeimg.com/640/480/tech",
-          email: "bluerainmango@gmail.com",
-          // image: { profileImg },
-          name: "Emily Yu"
-        }}
-        userView
-      />
-      <NavItem divider />
-      <NavItem
-        href="#anchor-about"
-        icon="sentiment_very_satisfied"
-        className="waves-effect"
-      >
-        About
-      </NavItem>
-      <NavItem href="#anchor-skill" icon="equalizer" className="waves-effect">
-        Skills
-      </NavItem>
-      <NavItem
-        href="#anchor-project"
-        icon="videogame_asset"
-        className="waves-effect"
-      >
-        Project
-      </NavItem>
-      <NavItem href="#anchor-experience" icon="work" className="waves-effect">
-        Experience
-      </NavItem>
-      <NavItem href="#anchor-education" icon="school" className="waves-effect">
-        Education
-      </NavItem>
-      <NavItem href="#anchor-contact" icon="email" className="waves-effect">
-        Contact
-      </NavItem>
-      <NavItem
-        href="https://raw.githubusercontent.com/bluerainmango/emilyYu/661e64e47ac1ac550cc454ce38acf29e0c8387fd/assets/resume_emilyYu.pdf"
-        target="_blank"
-        icon="insert_drive_file"
-        className="waves-effect"
-      >
-        Resume
-      </NavItem> */}
-      {/* <NavItem href="">Getting started</NavItem>
-    <NavItem href="components.html">Components</NavItem> */}
     </Navbar>
   </div>
 );
