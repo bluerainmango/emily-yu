2963a2a8d6eecfc18c822c511bee7d55b73a3e19
Emily Yu <bluerainmango@gmail.com>
2020-04-08 12:07:32 -0700

    added and changed skill logos: redux, jquery, react

--- src/components/skill/skill.component.jsx
+++ src/components/skill/skill.component.jsx
@@ -8,13 +8,14 @@ import bootstrap from "../../assets/img/bootstrap.png";
 import materialize from "../../assets/img/materialize.png";
 import js from "../../assets/img/js.png";
 import jquery from "../../assets/img/jquery.png";
-import react from "../../assets/img/react.png";
+import react from "../../assets/img/react.jpg";
+import redux from "../../assets/img/redux.png";
 import nodejs from "../../assets/img/nodejs.png";
 import express from "../../assets/img/express.png";
 import mysql from "../../assets/img/mysql.jpg";
 import sequelize from "../../assets/img/sequelize.jpg";
 import mongodb from "../../assets/img/mongodb.png";
-import mongoose from "../../assets/img/mongoose.png";
+import mongoose from "../../assets/img/mongoose.jpg";
 import webpack from "../../assets/img/webpack.png";
 import github from "../../assets/img/github.png";
 import heroku from "../../assets/img/heroku.png";
@@ -40,7 +41,7 @@ export const Skill = () => {
           title="HTML & CSS"
           images={[html5, css3, sass, bootstrap, materialize]}
         />
-        <SkillSlot title="Javascript" images={[js, jquery, react]} />
+        <SkillSlot title="Javascript" images={[js, jquery, react, redux]} />
         <SkillSlot
           title="Backend"
           images={[nodejs, express, mysql, sequelize, mongodb, mongoose]}
